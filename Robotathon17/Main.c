6eca6b0360e9933b4bac98a6bec5e5431cd658f0
ttak007 <tyler_takeyama@hotmail.com>
2017-10-12 14:02:14 -0700

    modified

--- Robotathon17/Main.c
+++ Robotathon17/Main.c
@@ -1,6 +1,7 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include <RASLib/inc/motor.h>
 
 // Blink the LED to show we're on
 tBoolean blink_on = true;
@@ -14,11 +15,13 @@ void blink(void) {
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
-    CallEvery(blink, 0, 0.5);
-
+    //CallEvery(blink, 0, 0.5);
+    tMotor *left = InitializeServoMotor(PIN_xx, true);
+    tMotor *right = InitializeServoMotor(PIN_xx, false);
     while (1) {
         // Runtime code can go here
-        Printf("Hello World!\n");
+        SetMotor(left, 1.0);
+        SetMotor(right, -1.0);
 
     }
 }
