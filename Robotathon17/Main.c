7eb543711b64eb56ae98adf678ac17a44f96e21a
TakumaFujiwara <tfujiwara2017@gmail.com>
2017-10-26 20:50:23 -0500

    Wall follow made on Thursday

--- Robotathon17/Main.c
+++ Robotathon17/Main.c
@@ -14,57 +14,62 @@ void blink(void) {
 }
 
 void clockwisemode(tMotor *left, tMotor *right, float distvalcw){
-  if(distvalcw<=0.35)
+  if(distvalcw<=0.30)
   {
-    SetMotor(left, 1.0);
-    SetMotor(right, -0.55);
+    SetMotor(left, -0.55);
+    SetMotor(right, -0.15);
   }
-  else if(0.35<distvalcw<=0.4)
+  else if(0.30<distvalcw<=0.35)
   {
-    SetMotor(left, 1.0);
-    SetMotor(right, -0.75);
+    SetMotor(left, -0.55);
+    SetMotor(right, -0.45);
   }
-  else if(0.4<distvalcw<=0.45)
+  /*else if(0.4<distvalcw<=0.45)
   {
     SetMotor(left, 1.0);
     SetMotor(right, -0.85);
   }
   else if(0.6>distvalcw>=0.55)
   {
-    SetMotor(left, 0.85);
-    SetMotor(right, -1.0);
-  }
-  else if(0.65>distvalcw>=0.6)
+    SetMotor(left, -0.85);
+    SetMotor(right, 1.0);
+  }*/
+  else if(0.50>distvalcw>=0.45)
   {
-    SetMotor(left, 0.75);
-    SetMotor(right, -1.0);
+    SetMotor(left, -0.45);
+    SetMotor(right, -0.55);
   }
-  else if(distvalcw>=0.65)
+  else if(distvalcw>=0.50)
   {
-    SetMotor(left, 0.55);
-    SetMotor(right, -1.0);
+    SetMotor(left, -0.15);
+    SetMotor(right, -0.55);
+  }
+  else if(0.45>distvalcw>0.35)
+  {
+    SetMotor(left, -0.55);
+    SetMotor(right, -0.55);
   }
   else
   {
-    SetMotor(left, 1.0);
-    SetMotor(right, -1.0);
+    SetMotor(left, 0.1);
+    SetMotor(right, -0.1);
   }
 }
 void counterclockwisemode(tMotor *left, tMotor *right, float distvalccw){
   if(distvalccw<=0.35)
   {
-    SetMotor(left, 0.55);
-    SetMotor(right, -1.0);
+    SetMotor(left, -0.55);
+    SetMotor(right, 1.0);
   }
   else if(0.35<distvalccw<=0.4)
   {
-    SetMotor(left, 0.75);
-    SetMotor(right, -1.0);
+    SetMotor(left, -0.75);
+    SetMotor(right, 1.0);
   }
   else if(0.4<distvalccw<=0.45)
   {
-    SetMotor(left, 0.85);
-    SetMotor(right, -1.0);
+    SetMotor(left, -0.85);
+    SetMotor(right, 1.0);
   }
   else if(0.6>distvalccw>=0.55)
   {
@@ -84,7 +89,7 @@ void counterclockwisemode(tMotor *left, tMotor *right, float distvalccw){
   else
   {
     SetMotor(left, 1.0);
-    SetMotor(right, -1.0);
+    SetMotor(right, 1.0);
   }
 }
 // The 'main' function is the entry point of the program
@@ -95,7 +100,7 @@ int main(void) {
     tMotor *right = InitializeServoMotor(PIN_D1, false);
     /*tLineSensor *line = InitializeGPIOLineSensor(PIN_D0, PIN_D1, PIN_D2, PIN_D3, PIN_D4, PIN_E1, PIN_E2, PIN_E1);
     float linevals[8];*/
-    tADC *dist = InitializeADC(PIN_B0);
+    tADC *dist = InitializeADC(PIN_B4);
     float distvalcw;
     tADC *dist2 = InitializeADC(PIN_B1);
     float distvalccw;
@@ -112,15 +117,19 @@ int main(void) {
         //Printf("IR sensor value is %f\n", distvalcw);
         distvalccw = ADCRead(dist2);
         //Printf("IR sensor value is %f\n", distvalccw);
-        if(0.55>=distvalcw>=0.45)
-        {
-          //CallEvery(blink, 0, 1.5); //determine if robot is in right spot??
-          clockwisemode(left,right,distvalcw);
+        SetMotor(left, 0.10);
+        SetMotor(right, -0.10);
+        if(1.0>=distvalcw>=0.0)
+        {while (1){
+          distvalcw = ADCRead(dist);
+          Printf("IR sensor value is %f\n", distvalcw);
+          CallEvery(blink, 0, 2.5); //determine if robot is in right spot??
+          clockwisemode(left,right,distvalcw);}
         }
-        else if(0.55>=distvalccw>=0.45)
-        {
-          counterclockwisemode(left,right,distvalccw);
-        }
+        /*else if(0.55>=distvalccw>=0.45)
+        {while(1)
+          {counterclockwisemode(left,right,distvalccw);}
+        }*/
 
         /*if(0.8>distval>0.5)
         {SetMotor(left, 1.0);
